41dba8ab155f3293e9aa3cf0f4d1ede8f08ad478
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-07-26 00:14:25 +0600

    Create Router class for work with routes. Fill in it with empty methods

---
+++ application/core/Router.php
@@ -0,0 +1,65 @@
+<?php
+/**
+	* An open source application development framework for PHP
+	* This content is released under the MIT License (MIT)
+	* Copyright (c) 2018, Kyrgyzstan, Bishkek, Alex Bowey
+	*
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in
+  * all copies or substantial portions of the Software.
+  *
+  * @package Moana
+  * @author Alex Bowey
+  * @copyright Copyright (c) 2018, Eshenov Taalaybek
+  * @license http://opensource.org/licenses/MIT	MIT License
+  * @link https://themoana.000webhostapp.com/ Home page / GitHub https://github.com/Taalaybek/moana
+  * @since Version 1.0.0
+  *
+	*/
+
+/**
+  * Router core library for management url addresses
+  *
+  * @package Moana
+  * @subpackage Router
+  * @category Core library
+  * @author Alex Bowey, https://github.com/Taalaybek
+  * @link https://github.com/Taalaybek/moana/wiki
+  */
+	
+		namespace application\core;
+
+		class Router
+		{
+			
+			protected $routes = [];
+
+			protected $params = [];
+
+			public function __construct()
+			{
+				
+			};
+
+			public function add()
+			{
+
+			};
+
+			public function verify()
+			{
+
+			};
+
+			public function run()
+			{
+
+			};
+		};
+		
