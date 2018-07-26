ac508199037be5237398dcf1d4fdf0a783440759
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-07-26 18:08:31 +0600

    Fill in some routes: accoutn and news

---
+++ application/config/routes.php
@@ -0,0 +1,21 @@
+<?php
+/**
+	* Array contains routes's names\
+	*
+	* @package Moana
+	* @subpackage routes config file
+	* @category config file
+	* @author Alex Bowey, https://github.com/Taalaybek
+	* @link https://github.com/Taalaybek/moana/wiki
+	*/
+		return [
+			'account/login' => [
+				'controller' => 'account',
+				'controller' => 'login'
+			],
+
+			'news/show' => {
+					'controller' => 'news',
+					'action' => 'show'
+			}
+		];
