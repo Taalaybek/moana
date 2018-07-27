9460851b699f179937669702fdf96eb479a0da5b
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-07-27 22:58:49 +0600

    Add describe in to AccountController.php and add newline at end of file. Add newline at end of file in to Controller.php, Router.php, MainController.php

--- application/controllers/AccountController.php
+++ application/controllers/AccountController.php
@@ -1,18 +1,26 @@
 <?php 
+/**
+  * Custom example controller
+  *
+  * @package Moana
+  * @category Custom controller
+  * @author Alex Bowey, https://github.com/Taalaybek
+  * @link https://github.com/Taalaybek/moana/wiki
+  */
 
-namespace application\controllers;
-use application\core\Controller;
+		namespace application\controllers;
+		use application\core\Controller;
 
-class AccountController extends Controller
-{
-	
-	public function loginAction()
-	{
-		debug($this->route);
-	}
+		class AccountController extends Controller
+		{
+			
+			public function loginAction()
+			{
+				debug($this->route);
+			}
 
-	public function registerAction()
-	{
-		debug($this->route);
-	}
-}
+			public function registerAction()
+			{
+				debug($this->route);
+			}
+		}
