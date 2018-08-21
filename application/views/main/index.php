17832d348e3c2bdfeee354df39b4725c6e3a85a6
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-08-22 01:30:48 +0600

    Create index.php for MainController.indexAction(). Display View.render() $title argument #1

---
+++ application/views/main/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Default index view layout
+ * @author https://github.com/Taalaybek
+ * @filesource https://github.com/Taalaybek/moana/tree/view/application/views/default/index.php
+ */
+echo $title; // Display View.render($title, $vars = []) argument
