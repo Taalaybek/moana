37ac7aa8c382dee48946cc1e5fd9b12a16868057
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-08-22 23:40:50 +0600

    Redesign comment code style in route config file #15

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,16 +1,39 @@
 <?php
-/**
-	* Array contains routes's names
-	*
-	* @package Moana
-	* @subpackage routes config file
-	* @category config file
-	* @author Alex Bowey, https://github.com/Taalaybek
-	* @link https://github.com/Taalaybek/moana/wiki
-	*/
-		return [
-			'' => [
-				'controller' => 'default',
-				'action' => 'index'
-			]
-		];
+
+/*
+| -------------------------------------------------------------------------
+| URI ROUTING
+| -------------------------------------------------------------------------
+| This file lets you re-map URI requests to specific controller functions.
+|
+| Typically there is a one-to-one relationship between a URL string
+| and its corresponding controller class/method. The segments in a
+| URL normally follow this pattern:
+|
+|	example.com/class/method/id/
+| example.com/class/method/
+| In some instances, however, you may want to remap this relationship
+| so that a different class/function is called than the one
+| corresponding to the URL.
+|
+| -------------------------------------------------------------------------
+| RESERVED ROUTES
+| -------------------------------------------------------------------------
+|
+| There are one reserved routes:
+|
+|	'' => [
+|		'controller' => 'default',
+|   'action' => 'index'
+| ]
+|
+| Examples:	my-controller/index	-> my_controller/index
+|		my-controller/my-method	-> my_controller/my_method
+ */
+
+return [
+	'' => [
+		'controller' => 'default',
+    'action' => 'index'
+  ]
+];
