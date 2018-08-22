9cf2857968f23ee5ee887d9ec2cce9d27d5a5531
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-08-22 23:51:39 +0600

    Update comments and remove package description used in core libraries(1-24 lines) #15

--- application/controllers/DefaultController.php
+++ application/controllers/DefaultController.php
@@ -1,32 +1,10 @@
 <?php
-/**
-  * An open source application development framework for PHP
-  * This content is released under the MIT License (MIT)
-  * Copyright (c) 2018, Kyrgyzstan, Bishkek, Alex Bowey
-  *
-  * Permission is hereby granted, free of charge, to any person obtaining a copy
-  * of this software and associated documentation files (the "Software"), to deal
-  * in the Software without restriction, including without limitation the rights
-  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  * copies of the Software, and to permit persons to whom the Software is
-  * furnished to do so, subject to the following conditions:
-  *
-  * The above copyright notice and this permission notice shall be included in
-  * all copies or substantial portions of the Software.
-  *
-  * @package Moana
-  * @author Alex Bowey
-  * @copyright Copyright (c) 2018, Eshenov Taalaybek
-  * @license http://opensource.org/licenses/MIT MIT License
-  * @link https://themoana.000webhostapp.com/ Home page / GitHub https://github.com/Taalaybek/moana
-  * @version 1.0.0
-  *
-  */
 
 /**
-  * Default controller
+  * Default example controller
   *
   * @package Moana
+  * @subpackage DefaultController
   * @category Custom controller
   * @author Alex Bowey, https://github.com/Taalaybek
   * @link https://github.com/Taalaybek/moana/wiki
@@ -39,8 +17,12 @@
 		{
 
 			/**
-        * Index custom method
-        * @method index method. Opened index page
+        * Index Page for this controller.
+        * Maps to the following URL:
+        *             https://example.com/
+        * Since this controller is set as the default controller in
+        * config/routes.php, it's displayed at http://example.com/
+        * @method indexAction
         * @version 1.0.0
         */
 					public function indexAction()
