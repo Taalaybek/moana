79d227e479c8f4885809b97738ec3a013024dff7
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-08-22 23:00:42 +0600

    Set package name in 3 of the index.php Write condition of using software 16/23 Change author name on @copyright annotation line Write file description on 37 line Delete comment on 47 line #15

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
 /**
+  * Moana
+  *
   * An open source application development framework for PHP
   * This content is released under the MIT License (MIT)
   * Copyright (c) 2018, Kyrgyzstan, Bishkek, Alex Bowey
@@ -14,9 +16,17 @@
   * The above copyright notice and this permission notice shall be included in
   * all copies or substantial portions of the Software.
   *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  * THE SOFTWARE.
+  *
   * @package Moana
   * @author Alex Bowey
-  * @copyright Copyright (c) 2018, Eshenov Taalaybek
+  * @copyright Copyright (c) 2018, Alex Bowey
   * @license http://opensource.org/licenses/MIT MIT License
   * @link https://themoana.000webhostapp.com/ Home page / GitHub https://github.com/Taalaybek/moana
   * @since Version 1.0.0
@@ -24,18 +34,17 @@
   */
 
 /**
-  * Receiver view for all requests
+  * Application initialization base file
   *
   * @package Moana
-  * @subpackage Router
-  * @category Broadcaster view
+  * @category application initialization file
   * @author Alex Bowey, https://github.com/Taalaybek
   * @link https://github.com/Taalaybek/moana/wiki
   */
     use application\core\Router;
     require 'application/lib/Dev_helper.php';
 
-    spl_autoload_register( function($class){ // Need to read about this function
+    spl_autoload_register( function($class){
       $path = str_replace('\\', '/', $class . ".php");
       if (file_exists($path)) {
         require $path;
