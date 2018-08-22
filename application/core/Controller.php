0b9061827b2355ff173e36b1d5b0034b62701bc5
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-08-23 00:21:10 +0600

    Update comments in Controller.php #15

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -1,5 +1,7 @@
 <?php 
 /**
+  * Moana
+  *
   * An open source application development framework for PHP
   * This content is released under the MIT License (MIT)
   * Copyright (c) 2018, Kyrgyzstan, Bishkek, Alex Bowey
@@ -14,9 +16,17 @@
   * The above copyright notice and this permission notice shall be included in
   * all copies or substantial portions of the Software.
   *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  * THE SOFTWARE.
+  *
   * @package Moana
   * @author Alex Bowey
-  * @copyright Copyright (c) 2018, Eshenov Taalaybek
+  * @copyright Copyright (c) 2018, Alex Bowey
   * @license http://opensource.org/licenses/MIT MIT License
   * @link https://themoana.000webhostapp.com/ Home page / GitHub https://github.com/Taalaybek/moana
   * @version 1.0.0
@@ -30,6 +40,7 @@
   * @category Core controller
   * @author Alex Bowey, https://github.com/Taalaybek
   * @link https://github.com/Taalaybek/moana/wiki
+  * @method render() including files
   */
   
     namespace application\core;
@@ -40,20 +51,18 @@
       
       /**
         * $Route property contains in itself $route variable gear from construct
-        * @param empty property
+        * @var array
         */
           public $route;
 
       /**
         * Contains View core library object
-        * @param Object
+        * @var object
         */
           public $view;
 
       /**
         * Class constructor
-        * @method's load routes config, library, take down new routes in to add method
-        * @version 1.0.0
         */
           public function __construct($route)
           {
