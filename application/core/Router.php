cdb87d6dd21acfff9218fa60f3d4a67209c6db06
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-09-03 23:17:15 +0600

    Checked indents in Router.php #39

--- application/core/Router.php
+++ application/core/Router.php
@@ -1,53 +1,53 @@
 <?php
 /**
-  * Moana
-  *
+	* Moana
+	*
 	* An open source application development framework for PHP
 	* This content is released under the MIT License (MIT)
 	* Copyright (c) 2018, Kyrgyzstan, Bishkek, Alex Bowey
 	*
-  * Permission is hereby granted, free of charge, to any person obtaining a copy
-  * of this software and associated documentation files (the "Software"), to deal
-  * in the Software without restriction, including without limitation the rights
-  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  * copies of the Software, and to permit persons to whom the Software is
-  * furnished to do so, subject to the following conditions:
-  *
-  * The above copyright notice and this permission notice shall be included in
-  * all copies or substantial portions of the Software.
-  *
-  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
-  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  * THE SOFTWARE.
-  *
-  * @package Moana
-  * @author Alex Bowey
-  * @copyright Copyright (c) 2018, Alex Bowey
-  * @license http://opensource.org/licenses/MIT	MIT License
-  * @link https://themoana.000webhostapp.com/ Home page / GitHub https://github.com/Taalaybek/moana
-  * @version 1.0.0
-  *
+	* Permission is hereby granted, free of charge, to any person obtaining a copy
+	* of this software and associated documentation files (the "Software"), to deal
+	* in the Software without restriction, including without limitation the rights
+	* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	* copies of the Software, and to permit persons to whom the Software is
+	* furnished to do so, subject to the following conditions:
+	*
+	* The above copyright notice and this permission notice shall be included in
+	* all copies or substantial portions of the Software.
+	*
+	* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	* FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+	* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	* THE SOFTWARE.
+	*
+	* @package Moana
+	* @author Alex Bowey
+	* @copyright Copyright (c) 2018, Alex Bowey
+	* @license http://opensource.org/licenses/MIT	MIT License
+	* @link https://themoana.000webhostapp.com/ Home page / GitHub https://github.com/Taalaybek/moana
+	* @version 1.0.0
+	*
 	*/
 
 /**
-  * Router core library for management url addresses
-  *
-  * @package Moana
-  * @subpackage Router
-  * @category Core library
-  * @author Alex Bowey, https://github.com/Taalaybek
-  * @link https://github.com/Taalaybek/moana/wiki
-  */
-	
+	* Router core library for management url addresses
+	*
+	* @package Moana
+	* @subpackage Router
+	* @category Core library
+	* @author Alex Bowey, https://github.com/Taalaybek
+	* @link https://github.com/Taalaybek/moana/wiki
+	*/
+
 		namespace application\core;
 
 		class Router
 		{
-			
+
 			/**
 				* Routes's names
 				* @var array
@@ -66,7 +66,7 @@
 					public function __construct()
 					{
 						$arr = require 'application/config/routes.php';
-						
+
 						foreach ( $arr as $key => $val ) {
 							$this->add( $key, $val );
 						}
@@ -76,7 +76,7 @@
 			/**
 				*	Add new route method
 				*	@param $route, $params
-        * @var array
+				* @var array
 				*/
 					public function add( $route, $params )
 					{
