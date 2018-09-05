2ba189c6dd4e769780e0aa5ce253225701f9fc7c
Taalaybek <taalaybekeshenov.96@gmail.com>
2018-09-06 00:57:49 +0600

    @Fixed indents for PSR-2 standards in lib/Example.php #39

--- application/lib/Example.php
+++ application/lib/Example.php
@@ -1,22 +1,23 @@
 <?php
 /**
-  * Example library
-  *
-  * @package Moana
-  * @subpackage DefaultController
-  * @category Custom controller
-  * @author Alex Bowey, https://github.com/Taalaybek
-  * @link https://github.com/Taalaybek/moana/wiki
-  */
-    namespace application\lib;
+ * Example library
+ *
+ * @package Moana
+ * @subpackage DefaultController
+ * @category Custom controller
+ * @author Alex Bowey, https://github.com/Taalaybek
+ * @link https://github.com/Taalaybek/moana/wiki
+ */
 
-    class Example
-    {
-      /**
-        * Output string for testing
-        */
-        public function output()
-        {
-          echo "Example lib was been initialized";
-        }
-    }
+namespace application\lib;
+
+class Example
+{
+  /**
+   * Output string for testing
+   */
+  public function output()
+  {
+    echo "Example lib was been initialized";
+  }
+}
